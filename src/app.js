f763b31347fbdf878cde37b2f73d3813a9b15a92
nealukumies <nea.lukumies@metropolia.fi>
2025-03-31 14:31:33 +0300

    Refactor API structure and add cat routes

--- src/app.js
+++ src/app.js
@@ -6,24 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api/v1', api);
-
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Welcome to my REST API!');
 });
 
-app.get('/api/v1/cat', (req, res) => {
-  const cat = {
-    cat_id: 246,
-    name: 'Köpi',
-    birthdate: '2024-09-12',
-    weight: 4,
-    owner: 'Ida',
-    image: 'https://loremflickr.com/320/240/cat',
-  };
-  res.json(cat);
-});
+app.use('/api/v1', api);
 
 export default app;
