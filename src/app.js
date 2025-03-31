3823cac2add77092e620183b9255000cbb83c5ff
nealukumies <nea.lukumies@metropolia.fi>
2025-03-31 14:20:22 +0300

    example files added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
+
+import api from './api/index.js';
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/v1', api);
 
 app.use('/public', express.static('public'));
 
