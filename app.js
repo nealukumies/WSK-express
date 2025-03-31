0f055cf39944bb4671352767eb527f6bdc497f81
nealukumies <nea.lukumies@metropolia.fi>
2025-03-31 13:24:42 +0300

    lesson1

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const hostname = '127.0.0.1';
+const app = express();
+const port = 3000;
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+app.get('/api/v1/cat', (req, res) => {
+  const cat = {
+    cat_id: 246,
+    name: 'Köpi',
+    birthdate: '2024-09-12',
+    weight: 4,
+    owner: 'Ida',
+    image: 'https://loremflickr.com/320/240/cat',
+  };
+  res.json(cat);
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
