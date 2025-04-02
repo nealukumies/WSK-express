4797e22cde8fb521d4e8f1d398fbb1e319f21354
nealukumies <nea.lukumies@metropolia.fi>
2025-04-02 09:31:23 +0300

    sharp

---
+++ src/api/middlewares.js
@@ -0,0 +1,23 @@
+import sharp from 'sharp';
+
+const createThumbnail = async (req, res, next) => {
+  console.log('todo: tee kuvakäsittely', req.file);
+  if (!req.file) {
+    next();
+    return;
+  }
+
+  let extension = 'jpg';
+  if (req.file.mimetype === 'image/png') {
+    // if (req.file.mimetype.includes('/png')) {
+    extension = 'png';
+  }
+
+  await sharp(req.file.path)
+    .resize(100, 100)
+    .toFile(`${req.file.path}_thumb.${extension}`);
+
+  next();
+};
+
+export default createThumbnail;
