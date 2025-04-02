0d13df836b94d7af04f240b8ece3097c028a26e2
nealukumies <nea.lukumies@metropolia.fi>
2025-04-02 10:19:18 +0300

    lesson4

--- src/middlewares.js
+++ src/middlewares.js
@@ -3,6 +3,7 @@ import sharp from 'sharp';
 const createThumbnail = async (req, res, next) => {
   console.log('todo: tee kuvakäsittely', req.file);
   if (!req.file) {
+    console.log(req.file);
     next();
     return;
   }
@@ -16,7 +17,7 @@ const createThumbnail = async (req, res, next) => {
   await sharp(req.file.path)
     .resize(100, 100)
     .toFile(`${req.file.path}_thumb.${extension}`);
-
+  console.log(req.file);
   next();
 };
 
